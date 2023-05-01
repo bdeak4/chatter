af4bd6e2a7552713464cd307c94f7521ed4cec50
Bartol Deak <b@bdeak.net>
2023-05-01 03:23:10 +0200

    add superjson

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,7 +1,10 @@
 import { initTRPC } from "@trpc/server";
+import superjson from "superjson";
 import { Context } from "./context";
 
-const t = initTRPC.context<Context>().create();
+const t = initTRPC.context<Context>().create({
+  transformer: superjson,
+});
 
 export const middleware = t.middleware;
 export const router = t.router;
