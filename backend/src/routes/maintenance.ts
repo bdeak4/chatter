29366c0cd504df4eb74a40dfebf63741bb3e406f
Bartol Deak <b@bdeak.net>
2023-05-01 05:12:13 +0200

    add procedure with input

--- backend/src/routes/maintenance.ts
+++ backend/src/routes/maintenance.ts
@@ -1,12 +1,15 @@
 import { publicProcedure, router } from "../trpc";
 import { z } from "zod";
+import { log } from "../logger";
 
 export const maintenanceRouter = router({
   healthz: publicProcedure.query(async ({ ctx }) => {
     await ctx.prisma.$queryRaw`SELECT 1`;
     return "ok";
   }),
-  echo: publicProcedure.input(z.string()).query(async (input) => {
-    return input;
-  }),
+  echo: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(async ({ input }) => {
+      return input.text;
+    }),
 });
