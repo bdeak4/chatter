db4a0afeb6231bcc77f3c066effd774683cee9f5
Bartol Deak <b@bdeak.net>
2023-05-01 16:34:05 +0200

    set default options for react-query

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,11 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
-import { createTRPCClient, trpc } from "../trpc";
+import { createQueryClient, createTRPCClient, trpc } from "../trpc";
 import { HomePage } from "./home";
 
 function App() {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => createQueryClient());
   const [trpcClient] = useState(() => createTRPCClient());
 
   return (
@@ -13,6 +13,7 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <h1>Vite + React</h1>
         <HomePage />
+
         {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
       </QueryClientProvider>
     </trpc.Provider>
