f653d5bd32c8f593cc41a351717e3056ab15bf5b
Bartol Deak <b@bdeak.net>
2023-05-04 21:00:56 +0200

    add promtail log transporter

--- backend/src/logger.ts
+++ backend/src/logger.ts
@@ -1,8 +1,9 @@
 import pino from "pino";
 import { env } from "./env";
 
-const transport = pino.transport({
-  target: env.NODE_ENV === "development" ? "pino-pretty" : "pino/file",
-});
+const transport =
+  env.NODE_ENV === "development"
+    ? pino.transport({ target: "pino-pretty" })
+    : undefined;
 
 export const log = pino(transport);
